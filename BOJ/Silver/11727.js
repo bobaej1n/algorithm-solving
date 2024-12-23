c10c1963c75c495878cfaeb48852506e8a8c18ef
bobaejin <bo_bae_jin@naver.com>
2024-12-23 11:07:09 +0900

    ✨ 2xn 타일링 2

---
+++ BOJ/Silver/11727.js
@@ -0,0 +1,15 @@
+// 2xn 타일링 2
+
+const input = Number(require("fs").readFileSync(0, "utf8").trim());
+
+const arr = Array(input + 1).fill(0);
+
+arr[1] = 1;
+arr[2] = 3;
+
+for (let i = 3; i <= input; i++)
+{
+  	arr[i] = (arr[i - 1] + arr[i - 2] * 2) % 10007;
+}
+
+console.log(arr[input]);  
