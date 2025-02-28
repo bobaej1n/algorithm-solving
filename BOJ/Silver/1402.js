30d4c457cd3e6209f6e1fe6c2cedac04e2d6e9aa
bobaejin <bo_bae_jin@naver.com>
2025-02-28 10:27:18 +0900

    ✨ 아무래도이문제는A번난이도인것같다

---
+++ BOJ/Silver/1402.js
@@ -0,0 +1,9 @@
+// 아무래도이문제는A번난이도인것같다
+
+const input = require("fs").readFileSync("input.txt", "utf8").trim().split("\n");
+let repeat = +input.shift();
+
+while (repeat--)
+{
+	console.log("yes");
+}
