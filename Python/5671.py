efc31638564ef707dd466442a4370f4dc86d29a8
bobaejin <bo_bae_jin@naver.com>
2025-04-22 09:03:43 +0900

    ✨ 호텔 방 번호

---
+++ Python/5671.py
@@ -0,0 +1,21 @@
+# 호텔 방 번호
+
+while 1:
+    try:
+        N, M = map(int, input().split())
+    except:
+        break
+    
+    ans = 0
+    
+    for num in range(N, M + 1):
+        cnt = set()
+        s_num = str(num)
+        
+        for char in s_num:
+            cnt.add(char)
+            
+        if len(s_num) == len(cnt):
+            ans += 1
+            
+    print(ans)
