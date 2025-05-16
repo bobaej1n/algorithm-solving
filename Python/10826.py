921d372c5bc788a0e180d9d76348c7faf7155c6d
bobaejin <bo_bae_jin@naver.com>
2025-05-16 10:14:11 +0900

    ✨ 피보나치 수 4

---
+++ Python/10826.py
@@ -0,0 +1,18 @@
+# 피보나치 수 4
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+        
+    return b
+        
+n = int(input())
+
+print(fibo(n))
